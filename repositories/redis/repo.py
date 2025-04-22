d8d4530197de15299e69278b14744a546bbd5b7e
OtaviOuu <conta.otaviou@gmail.com>
2025-04-22 15:19:54 -0300

    Implement RedisRepository class with insert method for key-value storage

---
+++ repositories/redis/repo.py
@@ -0,0 +1,12 @@
+from redis import Redis
+
+
+class RedisRepository:
+    # service → repository → db (e usa model como estrutura)
+
+    def __init__(self, redis_connection: Redis) -> None:
+        # injeção de dependência do redis_connection
+        self.__redis_connection = redis_connection
+
+    def insert(self, key: str, value: any) -> None:
+        self.__redis_connection.set(key, value)
