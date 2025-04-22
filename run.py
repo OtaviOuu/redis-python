917a66e45177b9b8ea61ae96a7a2a7455f60af1a
OtaviOuu <conta.otaviou@gmail.com>
2025-04-22 15:19:59 -0300

    Add RedisRepository usage to insert key-value pair in run.py

--- run.py
+++ run.py
@@ -1,4 +1,11 @@
 from models.connection.redis.connection import RedisConnectionHandle
 
+from repositories.redis.repo import RedisRepository
+
 redis_connection = RedisConnectionHandle().connect()
-print(redis_connection)
+redis_repository = RedisRepository(redis_connection)
+
+redis_repository.insert("key1", "value1")
+
+
+
